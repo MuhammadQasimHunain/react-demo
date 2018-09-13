467ab0d4e46f3a83f24a9932f4450fe07d6ffc9a
MUHAMMAD QASIM HUNAIN <muhammad.qasim1255@hotmail.com>
2018-09-13 17:58:18 +0500

    demo

--- ReactDemo/ReactDemo/ClientApp/components/NavMenu.tsx
+++ ReactDemo/ReactDemo/ClientApp/components/NavMenu.tsx
@@ -12,7 +12,7 @@ export class NavMenu extends React.Component<{}, {}> {
                         <span className='icon-bar'></span>
                         <span className='icon-bar'></span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>Markinson Demo</Link>
+                    <Link className='navbar-brand' to={ '/' }>React Demo</Link>
                 </div>
                 <div className='clearfix'></div>
                 <div className='navbar-collapse collapse'>
